9df2f95b641b2653cdc22c6722689b6d60e76c5b
Hennedy <Kunaragi@users.noreply.github.com>
2017-02-20 11:58:51 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,25 @@
 <html>
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+
     <title>BattleGo</title>
+
+<link rel="stylesheet" media="screen" href="style.css">
+
+    <!-- CSS  -->
+       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+       <!-- Compiled and minified CSS -->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+
+       <!--  Scripts-->
+       <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+       <!-- Compiled and minified JavaScript -->
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
+
   </head>
 
   <body>
@@ -15,9 +33,12 @@
 
     <nav>
       <ul>
+        <div id="topic">
         <li><a href="signup.php">S'inscrire</a></li>
         <li><a href="login.php">Connexion</a></li>
       </ul>
+    </div>
+
     </nav>
 
     <hr />
